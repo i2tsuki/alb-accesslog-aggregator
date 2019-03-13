84c1056c5345ad4f464bae8c98cd33bc646b84d7
kizkoh <mac-daisuki@live.jp>
2019-03-14 05:36:29 +0900

    Remove `coding:` header

--- src/aggregator.py
+++ src/aggregator.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 import datetime
 from logging import getLogger, StreamHandler, INFO, ERROR
 
--- tools/set_log_retention.py
+++ tools/set_log_retention.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import argparse
 import sys
 from logging import getLogger, StreamHandler, ERROR
